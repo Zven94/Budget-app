d8fb38bbdfb3c274e8a05aa41755ee35d47fa735
Zven94 <nicobolanez@gmail.com>
2023-10-18 19:23:15 -0300

    add foreign keys

--- db/migrate/20231018221148_create_groups.rb
+++ db/migrate/20231018221148_create_groups.rb
@@ -3,6 +3,7 @@ class CreateGroups < ActiveRecord::Migration[7.1]
     create_table :groups do |t|
       t.string :name
       t.text :icon
+      t.reference :user, null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/migrate/20231018221247_create_entities.rb
+++ db/migrate/20231018221247_create_entities.rb
@@ -3,6 +3,7 @@ class CreateEntities < ActiveRecord::Migration[7.1]
     create_table :entities do |t|
       t.string :name
       t.float :amount
+      t.reference :user, null: false, foreign_key: true
       t.timestamps
     end
   end
