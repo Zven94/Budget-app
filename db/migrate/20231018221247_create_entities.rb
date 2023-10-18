afd7d57117e0db3269c8bbf26191ab82cd20c8f4
Zven94 <nicobolanez@gmail.com>
2023-10-18 19:16:43 -0300

    create schema

--- db/migrate/20231018221247_create_entities.rb
+++ db/migrate/20231018221247_create_entities.rb
@@ -1,7 +1,8 @@
 class CreateEntities < ActiveRecord::Migration[7.1]
   def change
     create_table :entities do |t|
-
+      t.string :name
+      t.float :amount
       t.timestamps
     end
   end
