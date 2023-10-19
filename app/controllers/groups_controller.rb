407318f46b570cc58f57456a4c5b39d47fb2a7ea
Zven94 <nicobolanez@gmail.com>
2023-10-19 00:51:08 -0300

    add form for categories

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,30 @@
 class GroupsController < ApplicationController
+  def index
+    @groups = Group.all
+  end
+  def new
+    @user = current_user
+    @group = Group.new
+  end
+  def create
+    group_params_without_user_id = group_params.except(:user_id)
+    @group = current_user.groups.build(group_params_without_user_id)
+    if @group.save
+      redirect_to user_groups_path(current_user)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_group
+    @group = Group.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def group_params
+    params.require(:group).permit(:name, :user_id)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
+  has_one_attached :icon
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-  has_many :entity
-  has_many :group
+  has_many :entities
+  has_many :groups
 
   validates :name, presence: true
 end
